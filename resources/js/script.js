3767acd87aa5046c7424a85056ed963f68fff847
Calvin Christian <calvinchrst@gmail.com>
2019-02-23 11:37:49 +0800

    Add hamburger navigation for screen width <767px

--- resources/js/script.js
+++ resources/js/script.js
@@ -84,5 +84,20 @@ $(document).ready(function () {     // load jquery after the whole document is l
         offset: '50%'
     });
     
+    // Mobile navigation
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i')
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-ios-menu')) {
+            icon.addClass('ion-md-close')
+            icon.removeClass('ion-ios-menu')
+        } else {
+            icon.addClass('ion-ios-menu')
+            icon.removeClass('ion-md-close')
+        }
+    });
+    
 });
  
